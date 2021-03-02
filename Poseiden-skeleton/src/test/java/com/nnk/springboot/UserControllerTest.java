c4a6346dda2aba77e68525ff8182cccc378cfaac
Qaozer <gribardiere@gmail.com>
2021-03-02 19:22:15 +0100

    feature/controllerTests: additions to usercontrollerTest

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/UserControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/UserControllerTest.java
@@ -1,5 +1,8 @@
 package com.nnk.springboot;
 
+import com.nnk.springboot.Services.UserService;
+import com.nnk.springboot.domain.User;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -20,6 +25,9 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    @Autowired
+    private UserService userService;
 
     @WithMockUser(authorities = "ADMIN")
     @Test
@@ -45,7 +53,7 @@ public class UserControllerTest {
         this.mockMvc.perform(get("/user/add")).andExpect(status().isOk());
     }
 
-    /*@WithMockUser(authorities = "ADMIN")
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void testValidateUserAdmin() throws Exception{
         this.mockMvc.perform(post("/user/validate")
@@ -54,7 +62,7 @@ public class UserControllerTest {
                 .param("password", "password")
                 .param("role", "USER")
         ).andExpect(status().isOk());
-    }*/
+    }
 
     @WithMockUser
     @Test
@@ -64,6 +72,37 @@ public class UserControllerTest {
                 .param("username", "username")
                 .param("password", "password")
                 .param("role", "USER")
-        ).andExpect(status().isForbidden());
+        ).andExpect(status().isOk());
+    }
+
+    @WithMockUser(authorities = "ADMIN")
+    @Transactional
+    @Test
+    public void testShowUpdateUserAdmin() throws Exception{
+        User user = userService.add(new User("Username", "Fullname", "Password", RoleEnum.USER.getCode()));
+
+        this.mockMvc.perform(get("/user/update/"+user.getId()))
+                .andExpect(model().attribute("user", Matchers.hasProperty("username",Matchers.equalTo("Username"))))
+                .andExpect(model().attribute("user", Matchers.hasProperty("fullname",Matchers.equalTo("Fullname"))))
+                .andExpect(model().attribute("user", Matchers.hasProperty("password",Matchers.equalTo(""))))
+                .andExpect(model().attribute("user", Matchers.hasProperty("role",Matchers.equalTo(RoleEnum.USER.getCode()))));
+    }
+
+    @WithMockUser
+    @Transactional
+    @Test
+    public void testShowUpdateUser() throws Exception{
+        User user = userService.add(new User("Username", "Fullname", "Password", RoleEnum.USER.getCode()));
+
+        this.mockMvc.perform(get("/user/update/"+user.getId())).andExpect(status().isForbidden());
+    }
+
+    @WithMockUser(authorities = "ADMIN")
+    @Transactional
+    @Test
+    public void testDeleteUserAdmin() throws Exception{
+        User user = userService.add(new User("Username", "Fullname", "Password", RoleEnum.USER.getCode()));
+
+        this.mockMvc.perform(get("/user/delete/"+user.getId())).andExpect(status().isFound()).andReturn();
     }
 }
