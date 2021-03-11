03b51a3c92f084210ac8d912af08d7d1b73d839f
Qaozer <gribardiere@gmail.com>
2021-03-11 10:35:15 +0100

    feature/Cleanup: TradeService javadoc

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/Services/TradeService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/Services/TradeService.java
@@ -14,25 +14,48 @@ public class TradeService {
     @Autowired
     private TradeRepository tradeRepository;
 
+    /**
+     * Retrieves a list of Trades from database
+     * @return a list of Trades
+     */
     @Transactional(readOnly=true)
     public List<Trade> findAll(){ return tradeRepository.findAll();}
 
+    /**
+     * Saves a trade in database
+     * @param trade the trade
+     * @return the trade in database
+     */
     public Trade save(Trade trade){
         return tradeRepository.save(trade);
     }
 
+    /**
+     * Retrieves a trade from the database
+     * @param id the trade id
+     * @return the trade or an exception if the trade was not found
+     */
     @Transactional(readOnly=true)
     public Trade findById(int id){
         return tradeRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid trade Id:" + id));
     }
 
+    /**
+     * Updates a trade in database
+     * @param trade the updated trade
+     * @param id the trade id in database
+     * @return the trade in database
+     */
     public Trade update(Trade trade, int id){
         trade.setTradeId(id);
         return tradeRepository.save(trade);
     }
 
+    /**
+     * Deletes a trade from the database
+     * @param id the trade id
+     */
     public void delete(int id){
-        Trade trade = this.findById(id);
-        tradeRepository.delete(trade);
+        tradeRepository.deleteById(id);
     }
 }
