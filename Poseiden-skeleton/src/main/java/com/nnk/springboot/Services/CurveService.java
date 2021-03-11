5b1aad8f9a9215ec18417fa58dfe45f1c9c3d6db
Qaozer <gribardiere@gmail.com>
2021-03-11 10:19:12 +0100

    feature/Cleanup: CurveService javadoc

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/Services/CurveService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/Services/CurveService.java
@@ -14,26 +14,50 @@ public class CurveService {
     @Autowired
     private CurvePointRepository curvePointRepository;
 
+    /**
+     * Gets a list of curvePoints from the database
+     * @return a list of curvePoints
+     */
     @Transactional(readOnly=true)
     public List<CurvePoint> findAll(){
         return curvePointRepository.findAll();
     }
 
+    /**
+     * Saves a curvePoint in database
+     * @param curvePoint the curvePoint
+     * @return the curvePoint in database
+     */
     public CurvePoint save(CurvePoint curvePoint){
         return curvePointRepository.save(curvePoint);
     }
 
+    /**
+     * Finds a curvePoint by its id
+     * @param id the curvePoint id
+     * @return the curvePoint or an exception if it was not found
+     */
     @Transactional(readOnly=true)
     public CurvePoint findById(int id){
         return curvePointRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid curvePoint Id:" + id));
     }
 
+    /**
+     * Updates a curvePoint
+     * @param old the curvePoint in database
+     * @param updated the curvePoint with updated infos
+     * @return the curvePoint in database
+     */
     public CurvePoint update(CurvePoint old, CurvePoint updated){
         old.setTerm(updated.getTerm());
         old.setValue(updated.getValue());
         return curvePointRepository.save(old);
     }
 
+    /**
+     * Deletes a curvePoint
+     * @param curvePoint the curvePoint
+     */
     public void delete(CurvePoint curvePoint){
         curvePointRepository.delete(curvePoint);
     }
